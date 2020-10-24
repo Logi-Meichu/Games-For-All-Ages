646f272644ab6e8b133e8c71d89c8742d4f0bc01
chan9988 <kai9988ckc.cs07@nctu.edu.tw>
2020-10-24 12:12:51 +0800

    add ox game

---
+++ ox game/ox_game.cpp
@@ -0,0 +1,121 @@
+#include <bits/stdc++.h>
+#include <conio.h>
+#include <windows.h>
+
+using namespace std;
+
+int player=1; 
+
+int p[3][3];
+
+void init(){
+    memset(p,0,sizeof(p));
+    p[2][0]=0;
+    player=1;
+}
+
+void print_table(){
+    int i,j;
+    for(i=0;i<3;i++){
+        for(j=0;j<3;j++){
+            cout << p[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+
+bool check_result(){
+    int i,j;
+    bool ch=true;
+    bool ch1=false;
+    if(p[0][0]==1&&p[0][1]==1&&p[0][2]==1) cout << "player1 win" << endl;
+    else if(p[1][0]==1&&p[1][1]==1&&p[1][2]==1) cout << "player1 win" << endl;
+    else if(p[2][0]==1&&p[2][1]==1&&p[2][2]==1) cout << "player1 win" << endl;
+    else if(p[0][0]==1&&p[1][0]==1&&p[2][0]==1) cout << "player1 win" << endl;
+    else if(p[0][1]==1&&p[1][1]==1&&p[2][1]==1) cout << "player1 win" << endl;
+    else if(p[0][2]==1&&p[1][2]==1&&p[2][2]==1) cout << "player1 win" << endl;
+    else if(p[0][0]==1&&p[1][1]==1&&p[2][2]==1) cout << "player1 win" << endl;
+    else if(p[0][2]==1&&p[1][1]==1&&p[2][0]==1) cout << "player1 win" << endl;
+    else if(p[0][0]==2&&p[0][1]==2&&p[0][2]==2) cout << "player2 win" << endl;
+    else if(p[1][0]==2&&p[1][1]==2&&p[1][2]==2) cout << "player2 win" << endl;
+    else if(p[2][0]==2&&p[2][1]==2&&p[2][2]==2) cout << "player2 win" << endl;
+    else if(p[0][0]==2&&p[1][0]==2&&p[2][0]==2) cout << "player2 win" << endl;
+    else if(p[0][1]==2&&p[1][1]==2&&p[2][1]==2) cout << "player2 win" << endl;
+    else if(p[0][2]==2&&p[1][2]==2&&p[2][2]==2) cout << "player2 win" << endl;
+    else if(p[0][0]==2&&p[1][1]==2&&p[2][2]==2) cout << "player2 win" << endl;
+    else if(p[0][2]==2&&p[1][1]==2&&p[2][0]==2) cout << "player2 win" << endl;
+    else ch=false;
+    for(i=0;i<3;i++){
+        for(j=0;j<3;j++){
+            if(!p[i][j]){
+                ch1=true;
+            }
+        }
+        if(ch1) break;
+    }
+    if(ch){
+        cout << "press 0 to reset" << endl;
+    }
+    if(ch1==false){
+        cout << "Tie" << endl;
+    }
+    print_table();
+    return ch;
+}
+
+int main(){
+    init();
+    char input;
+    while(player){
+        check_result();
+        input=getch();
+        if(input=='\x1b'){
+            break;
+        }
+        if(input=='1'&&p[2][0]==0){
+            p[2][0]=player;
+            player=3-player;
+        }
+        else if(input=='2'&&p[2][1]==0){
+            p[2][1]=player;
+            player=3-player;
+        }
+        else if(input=='3'&&p[2][2]==0){
+            p[2][2]=player;
+            player=3-player;
+        }
+        else if(input=='4'&&p[1][0]==0){
+            p[1][0]=player;
+            player=3-player;
+        }
+        else if(input=='5'&&p[1][1]==0){
+            p[1][1]=player;
+            player=3-player;
+        }
+        else if(input=='6'&&p[1][2]==0){
+            p[1][2]=player;
+            player=3-player;
+        }
+        else if(input=='7'&&p[0][0]==0){
+            p[0][0]=player;
+            player=3-player;
+        }
+        else if(input=='8'&&p[0][1]==0){
+            p[0][1]=player;
+            player=3-player;
+        }
+        else if(input=='9'&&p[0][2]==0){
+            p[0][2]=player;
+            player=3-player;
+        }
+        else if(input=='0'){
+            init();
+        }
+        else{
+            cout << "Invalid input" << endl; 
+        }
+        
+        
+    }
+    return 0;
+}
